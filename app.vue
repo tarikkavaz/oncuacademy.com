a6a04a11eb907a045c6b3dc17ce5a5bf3b98ee71
Tarik Kavaz <tarikkavaz@gmail.com>
2023-03-20 14:48:26 +0300

    content

--- app.vue
+++ app.vue
@@ -5,14 +5,14 @@
         <div class="max-w-2xl mx-auto lg:mx-0">
           <img class="h-80" src="/images/logo.png" alt="Your Company" />
           <div class="hidden sm:mt-32 sm:flex lg:mt-16">
-            <div class="relative px-3 py-1 text-sm leading-6 text-gray-500 rounded-full ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+            <!-- <div class="relative px-3 py-1 text-sm leading-6 text-gray-500 rounded-full ring-1 ring-gray-900/10 hover:ring-gray-900/20">
               Detaylı bilgi almak için  <a href="#" class="font-semibold text-oncured-600 whitespace-nowrap"><span class="absolute inset-0" aria-hidden="true" />iletişime geçin. <span aria-hidden="true">&rarr;</span></a>
-            </div>
+            </div> -->
           </div>
           <h1 class="mt-24 text-4xl font-bold tracking-tight text-gray-900 sm:mt-10 sm:text-6xl">Öncü Academy</h1>
-          <p class="mt-6 text-lg leading-8 text-gray-600">We strive to foster a sense of curiosity and quest in our learners, encouraging them to push beyond the boundaries of what they know and discover new frontiers of knowledge.</p>
+          <h2 class="mt-6 text-lg leading-8 text-gray-600">We strive to foster a sense of curiosity and quest in our learners, encouraging them to push beyond the boundaries of what they know and discover new frontiers of knowledge.</h2>
           <div class="flex items-center mt-10 gap-x-6">
-            <a href="#" class="rounded-md bg-oncured-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-oncured-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-oncured-600">Bilgi Al</a>
+            <a href="#" class="rounded-md bg-oncured-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-oncured-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-oncured-600">Learn more</a>
             <!-- <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span aria-hidden="true">→</span></a> -->
           </div>
         </div>
@@ -29,9 +29,9 @@
       <div class="lg:col-span-2 lg:col-start-2 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-1 lg:gap-x-8 lg:px-8">
         <div class="lg:pr-4">
           <div class="lg:max-w-lg">
-            <p class="text-base font-semibold leading-7 text-oncured-600">Deploy faster</p>
-            <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better workflow</h1>
-            <p class="mt-6 text-xl leading-8 text-gray-700">Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas.</p>
+            
+            <h2 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Purpose</h2>
+            <p class="mt-6 text-base leading-8 text-gray-700">At Öncü Academy, we strive to create a world where learning seamlessly integrates with every individual's life, constantly adapting to their unique needs. We aspire to break down all barriers to learning, making knowledge accessible, personalized, and inclusive for all. Through our programs, modules, and courses, we seek to pioneer new frontiers in learning, empowering individuals, organizations, and communities to reach their full potential. By fostering a culture of lifelong learning, we aim to drive positive change and make a lasting impact on the world.</p>
           </div>
         </div>
       </div>
@@ -41,24 +41,26 @@
       <div class="lg:col-span-2 lg:col-start-2 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-1 lg:gap-x-8 lg:px-8">
         <div class="lg:pr-4">
           <div class="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
-            <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id.</p>
             <ul role="list" class="mt-8 space-y-8 text-gray-600">
               <li class="flex gap-x-3">
-                <CloudArrowUpIcon class="flex-none w-5 h-5 mt-1 text-oncured-600" aria-hidden="true" />
-                <span><strong class="font-semibold text-gray-900">Push to deploy.</strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</span>
+                <EyeIcon class="flex-none w-5 h-5 mt-1 text-oncured-600" aria-hidden="true" />
+                <span><strong class="font-semibold text-gray-900">Vision.</strong> We will be an academy that serves as a beacon of hope and inspiration for all who seek to grow, develop, and succeed. An academy that values diversity, equity, and inclusion and empowers learners to pursue their passions and interests with confidence and enthusiasm..</span>
               </li>
               <li class="flex gap-x-3">
-                <LockClosedIcon class="flex-none w-5 h-5 mt-1 text-oncured-600" aria-hidden="true" />
-                <span><strong class="font-semibold text-gray-900">SSL certificates.</strong> Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.</span>
+                <TrophyIcon class="flex-none w-5 h-5 mt-1 text-oncured-600" aria-hidden="true" />
+                <span><strong class="font-semibold text-gray-900">Values.</strong> 
+                <ul>
+                  <li><strong>Respect for human dignity:</strong> We treat all individuals with dignity and respect, regardless of their background, race, religion, gender, or sexual orientation.</li>
+                  <li><strong>Responsibility:</strong> We are responsible for our actions and their consequences. We act ethically and take responsibility for our decisions and their impact on others.</li>
+                  <li><strong>Honesty:</strong> We are honest and truthful in our dealings with others, whether in professional or personal relationships.</li>
+                  <li><strong>Fairness:</strong> We strive for fairness and justice in our interactions with others, and work to eliminate discrimination and bias in all forms.</li>
+                  <li><strong>Compassion:</strong> We show compassion and empathy towards others, and work to help those in need.</li>
+                </ul>
+                </span>
               </li>
-              <li class="flex gap-x-3">
-                <ServerIcon class="flex-none w-5 h-5 mt-1 text-oncured-600" aria-hidden="true" />
-                <span><strong class="font-semibold text-gray-900">Database backups.</strong> Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</span>
-              </li>
+              
             </ul>
-            <p class="mt-8">Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices hac adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.</p>
-            <h2 class="mt-16 text-2xl font-bold tracking-tight text-gray-900">No server? No problem.</h2>
-            <p class="mt-6">Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh. Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam turpis ipsum eu a sed convallis diam.</p>
+            
           </div>
         </div>
       </div>
@@ -70,11 +72,7 @@
     <div class="grid max-w-2xl grid-cols-1 mx-auto gap-y-16 gap-x-8 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
       <div class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
         <div class="lg:pr-4">
-          <div class="lg:max-w-lg">
-            <p class="text-base font-semibold leading-7 text-oncured-600">Deploy faster</p>
-            <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better workflow</h1>
-            <p class="mt-6 text-xl leading-8 text-gray-700">Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam eget aliquam. Quisque id at vitae feugiat egestas.</p>
-          </div>
+
         </div>
       </div>
       <div class="p-12 -mt-12 -ml-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden lg:object-cover">
@@ -83,7 +81,7 @@
       <div class="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
         <div class="lg:pr-4">
           <div class="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
-            <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id.</p>
+            
             <ul role="list" class="mt-8 space-y-8 text-gray-600">
               <li class="flex gap-x-3">
                 <CloudArrowUpIcon class="flex-none w-5 h-5 mt-1 text-oncured-600" aria-hidden="true" />
@@ -91,7 +89,7 @@
               </li>
               <li class="flex gap-x-3">
                 <LockClosedIcon class="flex-none w-5 h-5 mt-1 text-oncured-600" aria-hidden="true" />
-                <span><strong class="font-semibold text-gray-900">SSL certificates.</strong> Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.</span>
+                <span><strong class="font-semibold text-gray-900">Values.</strong> Respect for human dignity: We treat all individuals with dignity and respect, regardless of their background, race, religion, gender, or sexual orientation.</span>
               </li>
               <li class="flex gap-x-3">
                 <ServerIcon class="flex-none w-5 h-5 mt-1 text-oncured-600" aria-hidden="true" />
@@ -109,7 +107,7 @@
 </template>
 
 <script setup>
-import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/vue/20/solid'
+import { CloudArrowUpIcon, EyeIcon, TrophyIcon, LockClosedIcon, ServerIcon } from '@heroicons/vue/20/solid'
 useServerSeoMeta({
   title: 'Oncu Academy',
   ogTitle: 'Oncu Academy',
