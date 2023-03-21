a488aa8cd2e246d291d2d7158aea421212be4739
Tarik Kavaz <tarikkavaz@gmail.com>
2023-03-21 11:07:57 +0300

    static

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  target: 'static',
+  experimental: {
+    payloadExtraction: false
+  },
   head: {
     htmlAttrs: {
       lang: 'en'
