954ddaaf5e0e5dfc8400ef662300b1c2a68fd2c2
Tarik Kavaz <tarikkavaz@gmail.com>
2023-03-22 14:12:45 +0300

    head height

--- pages/Staff.vue
+++ pages/Staff.vue
@@ -1,13 +1,10 @@
 <template>
   <div>
-    <div class="relative bg-white" id="section-hero">
+    <div class="relative bg-white h-96" id="section-hero">
       <div class="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
         <div class="px-6 pt-10 pb-24 sm:pb-32 lg:col-span-7 lg:px-0 lg:pt-8 lg:pb-56 xl:col-span-6">
           <div class="max-w-2xl mx-auto lg:mx-0">
-            <img class="h-80" src="/images/logo.png" alt="Your Company" />
-            <div class="hidden sm:mt-32 sm:flex lg:mt-16">
-  
-            </div>
+            <img class="h-72" src="/images/logo.png" alt="Your Company" />
             <h1 class="mt-24 text-4xl font-bold tracking-tight text-gray-900 sm:mt-10 sm:text-6xl">Academic Staff</h1>
             <h2 class="mt-6 text-lg leading-8 text-gray-600">We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients.</h2>
           </div>
--- pages/Team.vue
+++ pages/Team.vue
@@ -1,13 +1,10 @@
 <template>
   <div>
-    <div class="relative bg-white" id="section-hero">
+    <div class="relative bg-white h-96" id="section-hero">
       <div class="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
         <div class="px-6 pt-10 pb-24 sm:pb-32 lg:col-span-7 lg:px-0 lg:pt-8 lg:pb-56 xl:col-span-6">
           <div class="max-w-2xl mx-auto lg:mx-0">
-            <img class="h-80" src="/images/logo.png" alt="Your Company" />
-            <div class="hidden sm:mt-32 sm:flex lg:mt-16">
-  
-            </div>
+            <img class="h-72" src="/images/logo.png" alt="Your Company" />
             <h1 class="mt-24 text-4xl font-bold tracking-tight text-gray-900 sm:mt-10 sm:text-6xl">Our Team</h1>
             <h2 class="mt-6 text-lg leading-8 text-gray-600">We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients.</h2>
           </div>
@@ -23,10 +20,6 @@
        
         <div class="py-24 bg-white sm:py-32">
           <div class="px-6 mx-auto max-w-7xl lg:px-8">
-            <div class="max-w-2xl mx-auto sm:text-center">
-              <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our leadership</h2>
-              <p class="mt-6 text-lg leading-8 text-gray-600">We’re a dynamic group of individuals who are passionate about what we do and dedicated to delivering the best results for our clients.</p>
-            </div>
             <ul role="list" class="grid max-w-2xl grid-cols-1 mx-auto mt-20 gap-x-6 gap-y-20 sm:grid-cols-2 lg:max-w-4xl lg:gap-x-8 xl:max-w-none">
               <li class="flex flex-col gap-6 xl:flex-row">
                 <img class="aspect-[4/5] w-52 flex-none rounded-2xl object-cover" src="https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="">
