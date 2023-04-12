07a8881f4e9db977f75a11be01defaff0973083c
Tarik Kavaz <tarikkavaz@gmail.com>
2023-04-12 10:51:01 +0300

    menu ring

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,15 +1,15 @@
 <template>
   <Popover class="relative flex justify-center">
     <PopoverButton class="items-center content-center mt-5 text-sm font-semibold leading-6 text-gray-900 gap-x-1">
-      <span class="inline-flex items-center self-end px-6 py-3 text-lg font-semibold leading-6 rounded-md shadow-lg text-slate-100 bg-oncured-800 gap-x-1">Menu </span>
+      <span class="inline-flex items-center self-end px-6 py-3 text-lg font-semibold leading-6 rounded-md shadow-lg shadow-oncured-800 text-slate-100 bg-oncured-800 gap-x-1">Menu </span>
     </PopoverButton>
 
     <transition enter-active-class="transition duration-200 ease-out" enter-from-class="translate-y-1 opacity-0" enter-to-class="translate-y-0 opacity-100" leave-active-class="transition duration-150 ease-in" leave-from-class="translate-y-0 opacity-100" leave-to-class="translate-y-1 opacity-0">
       <PopoverPanel class="absolute z-10 flex w-screen px-4 mt-20 -translate-x-1/2 left-1/2 max-w-max">
-        <div class="flex-auto w-screen max-w-md overflow-hidden text-lg leading-6 shadow-2xl bg-slate-100 rounded-3xl ring-1 ring-gray-900/5">
+        <div class="flex-auto w-screen max-w-md overflow-hidden text-lg leading-6 shadow-2xl bg-slate-100 rounded-3xl ring-4 ring-oncured-900/5">
           <div class="p-4">
             <div v-for="item in solutions" :key="item.name" class="relative flex p-4 rounded-lg group gap-x-6 hover:bg-slate-200">
-              <div class="flex items-center justify-center flex-none mt-1 rounded-lg h-11 w-11 bg-slate-50 group-hover:bg-slate-50 ">
+              <div class="flex items-center justify-center flex-none mt-1 rounded-lg h-11 w-11 bg-slate-50 group-hover:bg-slate-50">
                 <component :is="item.icon" class="w-6 h-6 text-gray-600 group-hover:text-oncured-600" aria-hidden="true" />
               </div>
               <div>
