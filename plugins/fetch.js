6295fdee52172ff132753e6098a7e30a137110d9
Tarik Kavaz <tarikkavaz@gmail.com>
2023-09-03 00:28:28 +0300

    tekrar

--- plugins/fetch.js
+++ plugins/fetch.js
@@ -2,6 +2,6 @@
 import axios from 'axios'
 
 export default async (context) => {
-  const response = await axios.get('https://www.bilgi.edu.tr/api/news/?site=test.bilgi.edu.tr')
+  const response = await axios.get('https://www.bilgi.edu.tr/api/news/?site=xxxx.bilgi.edu.tr')
   context.store.commit('setData', response.data)
 }
